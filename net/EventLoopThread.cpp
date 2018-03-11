2c2e0f91e6f92b095064b5a5d3728eb4abbbb1b2
bobbymly <1402278356@qq.com>
2018-03-11 12:29:28 +0800

    ServerLib_2018_3_11

---
+++ net/EventLoopThread.cpp
@@ -0,0 +1,48 @@
+#include "EventLoopThread.h"
+#include <functional>
+
+EventLoopThread::EventLoopThread():
+	loop_(NULL),
+	exiting_(false),
+	thread_(bind(&EventLoopThread::threadFunc,this),"EventLoopThread"),
+	mutex_(),
+	cond_(mutex_)
+{	}
+
+
+EventLoopThread::~EventLoopThread()
+{
+	exiting_=true;
+	if(loop_!=NULL)
+	{
+		loop->quit();
+		thread_.join();
+	}
+}
+
+EventLoop* EventLooopThread::startLoop()
+{
+	assert(!thread_.started());
+	thread_.start();
+
+	{
+		MutexLockGuard lock(mutex_);
+		while(loop==NULL)
+			cond_.wait();
+
+	}
+	return loop_;
+}
+
+void EventLoopThread::threadFunc()
+{
+	EventLoop loop;
+
+	{
+		MutexLockGuard lock(mutex_);
+		loop_=&loop;
+		cond_.notify();
+	}
+	loop.loop();
+	loop_=NULL;
+}
