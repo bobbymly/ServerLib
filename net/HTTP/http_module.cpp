6314ba37223db94f3fcb0b75f45f2354b81f3fc5
bobbymly <1402278356@qq.com>
2018-05-28 15:56:39 +0800

    latest

--- net/HTTP/http_module.cpp
+++ net/HTTP/http_module.cpp
@@ -25,7 +25,7 @@ bool http_module::processRequestLine(const char* begin,const char* end)
     {
         ++start;
         space =std::find(start,end,' ');
-        const char* question std::find(start,space,'?');
+        const char* question = std::find(start,space,'?');
         setPath(start,question);
         if(question != space)setQuery(question,space);
         ++start;
@@ -34,11 +34,11 @@ bool http_module::processRequestLine(const char* begin,const char* end)
         {
             if (*(end-1) == '1')
             {
-                request_.setVersion(HTTP_11);
+                setVersion(HTTP_11);
             }
             else if (*(end-1) == '0')
             {
-                request_.setVersion(HTTP_10);
+                setVersion(HTTP_10);
             }
             else
             {
@@ -54,12 +54,12 @@ bool http_module::parseHeaders(const char* begin,const char* end)
 {
     const char* start = begin;
     const char* step;
-    const char* equel_s;
+    const char* equal_s;
     while(start < end)
     {
-        step = std::equel(start,end,"\r\n");
-        equel_s = std::find(start,end,'=');
-        headers[string(start,equel_s)] = string(equel_s + 1,step);
+        step = std::equal(start,end,"\r\n");
+        equal_s = std::find(start,end,'=');
+        headers_[string(start,equal_s)] = string(equal_s + 1,step);
         start = step + 2;
     }
     return true;
@@ -70,7 +70,7 @@ bool http_module::parseHeaders(const char* begin,const char* end)
 bool http_module::parseRequest()
 {
     const char* start = buf_.data();
-    const char* request_line_end = buf_.find("\r\n");
+    const char* request_line_end = buf_.data() + buf_.find("\r\n");
     processRequestLine(start,request_line_end);
     //start += 2;
     const char* headers_end = std::find(buf_.data(),buf_.end(),"\r\n\r\n");
